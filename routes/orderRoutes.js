6aae48d641be630a488d60c0ed280c5616367033
szymonczopek <szymon.czopek@wp.pl>
2023-01-29 22:30:02 +0100

    routes and order & ordeitems constoller

---
+++ routes/orderRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { createOrder, getAllOrdersInRoom, getAllOrdersForUser, deleteOrder} = require('../controllers/orderController')
+const {verifyUser}= require('../middleware/verifyUser')
+
+const router= express.Router()
+
+
+
+router.get('/createOrder', verifyUser, createOrder)
+router.get('/getAllOrdersInRoom/:idRoom', verifyUser, getAllOrdersInRoom)
+router.get('/getAllOrdersForUser/:idRoom', verifyUser, getAllOrdersForUser)
+router.delete('/deleteOrder/:idOrder',verifyUser,deleteOrder)
+
+
+
+module.exports=router
