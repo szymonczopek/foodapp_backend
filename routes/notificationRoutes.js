b9eb79858fed400361322d2403cc1915a29c1dce
szymonczopek <szymon.czopek@wp.pl>
2023-01-29 21:52:04 +0100

    routes

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { getAll, reveiveNotification } = require('../controllers/notificationController')
+const { getAll, reveiveNotification, pushNotification, getNewNotifications, getNewInvitations } = require('../controllers/notificationController')
 const {verifyUser}= require('../middleware/verifyUser')
 
 const router= express.Router()
 
 router.get('/getAll',verifyUser,getAll)
-router.put('/setAsRead',verifyUser,reveiveNotification)
+router.put('/receiveNotification/:idNotification',verifyUser,reveiveNotification)
+router.post('/pushNotification',verifyUser,pushNotification)
+router.get('/getNewNotifications',verifyUser,getNewNotifications)
+router.get('/getNewInvitations',verifyUser,getNewInvitations)
 
 module.exports = router
+
