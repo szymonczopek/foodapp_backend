9b14e7a892884605ced78c82bc24a9681ea3522c
szymonczopek <szymon.czopek@wp.pl>
2023-01-29 17:09:33 +0100

    room memebers add

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const {verifyUser}= require('../middleware/verifyUser')
-const {createRoom} =require('../controllers/roomController')
+const {createRoom, getAll} =require('../controllers/roomController')
 
 const router= express.Router()
 
 router.post('/create',verifyUser,createRoom)
+router.post('/getAll',getAll)
 
 module.exports = router
